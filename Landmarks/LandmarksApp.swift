3fea630eb052c1d40cca8a77075bcf0d625ec4e4
Jose Castro <chepe.castro2312@gmail.com>
2023-06-23 23:11:10 -0600

    First commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jose Castro on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
